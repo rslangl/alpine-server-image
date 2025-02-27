e06b069bedb4cdcbac5dc9b0644c4e1da96c7283
rslangl <fjellape1447@protonmail.com>
2025-02-27 19:55:20 +0100

    fix: enable docker service

--- shell.nix
+++ shell.nix
@@ -7,4 +7,12 @@
   in pkgs.mkShell {
     packages = tools;
     inputsFrom = with pkgs; tools;
+
+    shellHook = ''
+      if ! pgrep -x "dockerd" > /dev/null; then
+        echo "Starting Docker daemon..."
+        sudo dockerd &
+        sleep 5
+      fi
+    '';
   }
